7b801be1709734366c5e4b3001730da5e47c816e
KhaosCoder <pancho.nugo@gmail.com>
2022-01-12 22:55:11 -0600

    parentheses

--- 75 Must Do Leetcode/20. Valid Parentheses/ValidParentheses.js
+++ 75 Must Do Leetcode/20. Valid Parentheses/ValidParentheses.js
@@ -2,6 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
- var isValid = function(s) {
-    
+var isValid = function(s) {
+    var pairs = { "(" : ")", "{" : "}", "[" : "]" };
+    let close = [")","]","}"];
+    let read = [];
+
+    let p=0;
+    for (p of s) {
+        if (close.indexOf(p) >= 0) {
+            if (pairs[read.pop()] != p)
+                return false;
+        } else read.push(p);
+    }
+    if (read.length > 0) return false;
+    return true;
 };
+
+console.log(isValid("()")); // true
+console.log(isValid("(){}[]")); //true
+console.log(isValid("(]")); // false
+console.log(isValid("{[]}")); // true
+
+// 12/Jan/2022
+// Runtime: 72 ms, faster than 86.16% of JavaScript online submissions for Valid Parentheses.
+// Memory Usage: 39.8 MB, less than 46.89% of JavaScript online submissions for Valid Parentheses.
